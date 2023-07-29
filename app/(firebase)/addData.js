da48c7d430de0eb4623b063834a7d36780e776a2
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-29 18:40:40 +0530

    added data additon

---
+++ app/(firebase)/addData.js
@@ -0,0 +1,18 @@
+import { getFirestore,doc,setDoc, projectFirestore } from "./config";
+
+export default async function addData(collection,id,data)
+{
+    let result = null; 
+    let error = null;
+    try {
+        result = await setDoc(doc(projectFirestore,collection,id),data,{
+            merge:true;
+        });
+    }
+    catch(e)
+    {
+        error = e
+    }
+
+    return {result,error};
+}
--- app/admin/page.js
+++ app/admin/page.js
@@ -2,15 +2,34 @@
 import React from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import addData from "../(firebase)/addData";
 function Page() {
     const { user } = useAuthContext()
     const router = useRouter()
+    const handleForm = async()=>{
+        const data = {
+            name:"John Snow",
+            house:"Stark"
+        }
+        const {result,error} = await addData('users','user-id',data)
+        if(error)
+        {
+            return console.log(error)
+        }
+        else {
+            alert("Your vote has been casted")
+        }
+    }
 
     React.useEffect(() => {
         if (user == null) router.push("/")
     }, [user])
 
-    return (<h1>Only logged in users can view this page</h1>);
+    return (<h1>
+        Only logged in users can view this page
+        {handleForm}
+
+    </h1>);
 }
 
 export default Page;
