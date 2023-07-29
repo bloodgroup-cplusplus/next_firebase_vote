82014bf496b011cf9338ee32ce6a493e061b50e0
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-29 23:11:16 +0530

    added required field to required inputs on form

--- app/admin/page.js
+++ app/admin/page.js
@@ -92,7 +92,7 @@ function Page() {
                   <div>
 
                   <label htmlFor="designation" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Vote</label>
-                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"   value={vote} onChange={(e)=>setVote(e.target.value)}>
+                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"   value={vote} onChange={(e)=>setVote(e.target.value)} required>
                       <option disabled selected>Select</option>
                           <option>SKM</option>
                           <option>SDF</option>
@@ -108,7 +108,7 @@ function Page() {
                       <label htmlFor="district" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                         District (School)
                       </label>
-                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  value={district} onChange={(e)=>setDistrict(e.target.value)}>
+                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  value={district} onChange={(e)=>setDistrict(e.target.value)} required>
                             <option disabled selected>Select</option>
                             <option>Gangtok</option>
                             <option>Geyzing</option>
